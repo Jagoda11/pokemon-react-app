d940df128eee8bb1ea43c50fa1d5df503147be09
Jagoda11 <jagoda.cubrilo@yahoo.com>
2024-06-19 17:25:20 +0200

    chore: Update Router basename for GitHub Pages deployment

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ import PokemonDetail from './components/PokemonDetail'
 
 const App: React.FC = () => {
   return (
-    <Router>
+    <Router basename="/pokemon-react-app">
       <Routes>
         <Route path="/" element={<PokemonList />} />
         <Route path="/pokemon/:name" element={<PokemonDetail />} />
